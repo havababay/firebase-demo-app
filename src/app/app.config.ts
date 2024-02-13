ea101750190e71f8eb2b1a27c5030f8b459df833
havababay <hava.babay@gmail.com>
2024-02-13 11:19:28 +0200

    first commit & auth guards

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), 
+    provideAnimationsAsync(), 
+    importProvidersFrom(
+      provideFirebaseApp(() => 
+        initializeApp({
+          "projectId":"fir-demo-app-210f8",
+          "appId":"1:258607751104:web:7395989837990a9f5ff225",
+          "storageBucket":"fir-demo-app-210f8.appspot.com",
+          "apiKey":"AIzaSyCzeJ6PU4Y6CtkTfmNT_OQiCBrN4ugepiA",
+          "authDomain":"fir-demo-app-210f8.firebaseapp.com",
+          "messagingSenderId":"258607751104",
+          "measurementId":"G-5KS9MCPTL4"}))), 
+        importProvidersFrom(
+          provideAuth(() => getAuth())), 
+        importProvidersFrom(
+          provideFirestore(() => getFirestore())), 
+        importProvidersFrom(
+          provideFunctions(() => getFunctions())), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"fir-demo-app-210f8","appId":"1:258607751104:web:7395989837990a9f5ff225","storageBucket":"fir-demo-app-210f8.appspot.com","apiKey":"AIzaSyCzeJ6PU4Y6CtkTfmNT_OQiCBrN4ugepiA","authDomain":"fir-demo-app-210f8.firebaseapp.com","messagingSenderId":"258607751104","measurementId":"G-5KS9MCPTL4"}))), importProvidersFrom(provideAuth(() => getAuth()))]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { ReservationsListComponent } from './reservations-list/reservations-list.component';
+import { canActivate  , redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 
-export const routes: Routes = [];
+const redirectToSignIn = () => redirectUnauthorizedTo(['sign-in']);
+
+export const routes: Routes = [
+    {
+        path: "sign-in", 
+        component: SignInComponent
+    },
+    {
+        path: "", 
+        component: ReservationsListComponent,
+        ...canActivate(redirectToSignIn)
+    },
+];
